2bd0703e3ccc54574642040152fe099d5a724396
khairajani <himanshukhairajani8@gmail.com>
2020-12-18 23:55:38 +0530

    main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# from inference import *
-# image_path = "./data/input/surf.jpg"
-# start_inference(image_path=image_path)
+from inference import *
+image_path = "./data/input/surf.jpg"
+start_inference(image_path=image_path)
 
-from inference_video import *
-video_path = "./data/input/guitar.mp4"
-start_inference(video_path=video_path)
+# from inference_video import *
+# video_path = "./data/input/guitar.mp4"
+# start_inference(video_path=video_path)
